54bdabb67f90cb2d365d18a84f3bc32e70138e1b
x1215808128-eng <x1215808128-eng@users.noreply.github.com>
2026-06-21 19:46:29 +0800

    Fix GTM snippet placement for Search Console verification.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
-import Script from "next/script";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { buildMetadata } from "@/lib/seo";
 import { SITE } from "@/lib/site";
 import "./globals.css";
+
+const GTM_ID = "GTM-KG49FCHN";
+
+const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`;
 
 const inter = Inter({
   subsets: ["latin"],
@@ -34,18 +41,12 @@ export default function RootLayout({
       className={`${inter.variable} ${spaceGrotesk.variable} h-full`}
     >
       <head>
-        <Script id="google-tag-manager" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KG49FCHN');`}
-        </Script>
+        <script dangerouslySetInnerHTML={{ __html: GTM_SCRIPT }} />
       </head>
       <body className="flex min-h-full flex-col bg-background antialiased text-foreground">
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KG49FCHN"
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
